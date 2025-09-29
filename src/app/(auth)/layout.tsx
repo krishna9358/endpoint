e59336b8c50232625f092e26777b649a49608025
Krishna Mohan <krishanmohank974@gmail.com>
2025-09-29 11:55:50 +0530

    layout created

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,17 @@
 import React from "react";
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+import { auth} from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect("/");
+  }
   return <div>{children}</div>;
 };
 
 export default AuthLayout;
+

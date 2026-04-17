d37f91b38272fc20b8a44579abb1091842b9c2f6
Krishna Mohan <krishanmohank974@gmail.com>
2026-04-18 00:46:06 +0530

    feat: created the workspace button,and added tanstack query to create workspace with backend.
    Signed-off-by: Krishna Mohan <krishanmohank974@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import ReactQueryProvider from "@/providers/react-query-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <Toaster/>
             {children}
           </ThemeProvider>
         </ReactQueryProvider>
--- src/components/workspace/create-workspace.tsx
+++ src/components/workspace/create-workspace.tsx
@@ -1,1 +1,49 @@
 "use client";
+
+import { useState } from "react";
+import Modal from "../ui/modal";
+import { Input } from "../ui/input";
+import { useCreateWorkspace } from "@/hooks/workspace/workspace";
+import { toast } from "sonner";
+
+
+const CreateWorkspace = ({ isModalOpen, setIsModalOpen }: { isModalOpen: boolean, setIsModalOpen: (open: boolean) => void }) => {
+  const [name, setName] = useState("");
+  const {mutateAsync : createWorkspaces, isPending} = useCreateWorkspace();
+  
+  const handleSubmit = async () => {
+    if(!name.trim()) return;
+    try{
+        await createWorkspaces(name);
+        toast.success("Workspace successfully created!")
+        setIsModalOpen(false);
+        setName("");
+    }catch(e){
+        toast.error("Failed to create workspace")
+    }
+    
+  } 
+  return (
+    <Modal 
+      title="Create Workspace" 
+      description="Add a new workspace to organize your projects" 
+      isOpen={isModalOpen} 
+      onClose={() => setIsModalOpen(false)}
+      onSubmit={handleSubmit}
+      submitText = {isPending ? "Creating..." : "Create Workspace"}
+      submitVariant="default">
+        <div className="space-y-4">
+            <Input
+                placeholder="Workspace name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full p-2 border rounded-sm"
+            />
+        </div>
+    </Modal>
+  )
+
+    
+}
+
+export default CreateWorkspace;

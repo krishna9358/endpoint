2f2bb2f7eb042a869d2b648c6d98de37c162a6f7
Krishna Mohan <krishanmohank974@gmail.com>
2026-05-02 16:01:05 +0530

    feat: save request feature is completed
    Signed-off-by: Krishna Mohan <krishanmohank974@gmail.com>

--- src/hooks/requests/request.ts
+++ src/hooks/requests/request.ts
@@ -7,15 +7,19 @@ import {
   addRequestToCollection,
   type Request,
 } from "@/actions/requests/index";
+import { useRequestPlaygroundStore } from "@/store/request/useRequestStore";
 
 // Add request to collection
 export function useAddRequestToCollection(collectionId: string) {
   const queryClient = useQueryClient();
+  const {updateTabFromSavedRequest, activeTabId} = useRequestPlaygroundStore();
   return useMutation({
     mutationFn: (request: Request) =>
       addRequestToCollection(collectionId, request),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["requests"] });
+      // @ts-ignore
+      updateTabFromSavedRequest(activeTabId!, data)
     },
     onError: (error) => {
       toast.error(error.message);
@@ -34,11 +38,15 @@ export const useGetRequests = (collectionId: string) => {
 // Save request
 export function useSaveRequest(id:string) {
   const queryClient = useQueryClient();
+  const {updateTabFromSavedRequest, activeTabId} = useRequestPlaygroundStore();
   return useMutation({
     mutationFn: ( request: Request) =>
       saveRequest(id, request),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["requests"] });
+      // @ts-ignore
+      updateTabFromSavedRequest(activeTabId!, data)
+
     },
     onError: (error) => {
       toast.error(error.message);
